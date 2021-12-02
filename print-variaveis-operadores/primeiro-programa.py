b1ddce808fb9c681b939ec8d0be2e2546e8031fe
unknown <alisson-mu@hotmail.com>
2021-12-02 02:04:43 -0300

    primeiro commit

---
+++ print-variaveis-operadores/primeiro-programa.py
@@ -0,0 +1,17 @@
+print("oi")
+
+print("Hello World")
+
+a = "alisson"
+b = 10
+c = 3.25
+
+print(type(a)) #tipo da variavel 'a'
+print(type(b)) #tipo da variavel 'b'
+print(type(c)) #tipo da variavel 'c'
+
+lista = [1,2,3,4,5, 'curso de python']
+
+print(lista)
+
+print( 'o valor de b é',b,'e o valor de c é',c )
